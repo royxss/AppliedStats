a4bc38155ee8176d1f8b83b874bee305c47b9d1a
royxss <findroy@outlook.in>
2017-11-19 12:16:12 -0600

    New version

--- ypll.R
+++ ypll.R
@@ -1,22 +1,24 @@
+setwd("C:\\Users\\SROY\\Desktop\\Courses\\Applied Stats\\Project")
 rm(list=ls())
 seedVal = 17869
 options(warn=-1)
 options(scipen=999)
 
+# Libraries
 library(ggplot2)
 library(ggcorrplot)
 library(dummies)
-library(rms)
-library(lmtest)
 
 measures <- read.csv2("additional_measures_cleaned.csv", header = TRUE, sep = ',')
 ypll <- read.csv2("ypll.csv", header = TRUE, sep = ',')
 
-head(measures)
-head(ypll)
+str(measures)
+str(ypll)
+
 df <- merge(x = ypll, y = measures, by.x = c("FIPS"), by.y = c("FIPS"))
-str(df)
+
 apply(df, 2, function(x) length(which(x == "" | is.na(x) | x == "NA")))
+
 # remove records that are unreliable
 df <- df[df$Unreliable != 'x',names(df) != 'Unreliable']
 # Remove cumulative records
@@ -29,9 +31,7 @@ df <- df[!is.na(df$YPLL.Rate),]
 df <- df[!df$Rural == '',]
 df <- df[!df$X..child.Illiteracy == '',]
 df <- df[!is.na(df$X..Free.lunch),]
-df <- df[(df$County.x)!='',]
 
-apply(df, 2, function(x) length(which(x == "" | is.na(x) | x == "NA")))
 # HIV rate is an important predictor. but 22% values are missing
 # so it is difficult to impute
 # we can bucket it into categories
@@ -39,26 +39,16 @@ HIVRate <- df[!is.na(df$HIV.rate),'HIV.rate']
 range(HIVRate)
 quantile(HIVRate)
 
-# we need to convert HiveRate to %HIV multiplying it by 100 more as binning requires int not num
-percentHIV <- HIVRate/df$Population*100*100
-range(percentHIV)
-quantile(percentHIV)
-length(percentHIV)
-length(df$FIPS)
-#df$HIV <- cut(df$HIV.rate, breaks = c(-Inf, 57, 98, 194, Inf), 
-#              labels = c("VeryLow","Low","High","VeryHigh"))
-
-df$HIV <- cut(percentHIV, breaks = c(-Inf, 12, 34, 94, Inf), 
+df$HIV <- cut(df$HIV.rate, breaks = c(-Inf, 57, 98, 194, Inf), 
               labels = c("VeryLow","Low","High","VeryHigh"))
-
 df[is.na(df$HIV),'HIV'] <- 'NotAvail'
-summary(df)
-names(df)
-str(df)
 
+# Remove irrelevant columns
+names(df)
 excludeList <- c("FIPS", "State.x", "State.y", "County.x", "County.y", "HIV.rate")
 includeList <- names(df[!names(df) %in% c(excludeList)])
 df <- df[,includeList]
+
 # Change factors to numeric
 df$X..18 <- as.numeric(as.character(df$X..18))
 df$X65.and.over <- as.numeric(as.character(df$X65.and.over))
@@ -68,6 +58,7 @@ df$Rural <- as.numeric(as.character(df$Rural))
 df$X..child.Illiteracy <- as.numeric(as.character(df$X..child.Illiteracy))
 str(df)
 summary(df)
+
 # select numeric columns
 numVars <- names(which(sapply(df, is.numeric)))
 # correlation matrix
@@ -86,14 +77,12 @@ ggplot(df, aes(YPLL.Rate)) +
   geom_histogram(fill="tomato3") +  
   labs(title="Histogram of YPLL Rate")
 # not highly skewed. No need to scale. We can verify it later.
+
 # Scatterplot
 ggplot(df, aes(x=Population, y=YPLL.Rate)) + 
   geom_point() + 
   geom_smooth(method="loess", se=F) + scale_x_log10() +
   labs(x="Population scaled to log 10")
-
-
-# Outliers
 
 # Dummy HIV variable
 df <- cbind(df, dummy(df$HIV))
@@ -113,133 +102,102 @@ createModelFormula <- function(yVar, xVars, includeIntercept = TRUE){
 }
 
 # Fit linear model with all variables
-modelForm <- createModelFormula(yVar = yVar, xVars = xVars, includeIntercept = TRUE)
+modelForm <- createModelFormula(yVar = yVar, xVars = xVars, includeIntercept = FALSE)
 modelForm
-model <- lm(modelForm, data = df)
-summary(model)
-plot(model)
+lm.1 <- lm(modelForm, data = df)
+#summary(lm.1)
+# Residual analysis:
+par(mfrow = c(2, 2))
+plot(lm.1)
 
 
-#Test for constant variance
-summary(lm(abs(residuals(model)) ~ fitted(model)))
+# Remove outliers using studentized residual
+stud_resid <- rstudent(lm.1)
+hist(stud_resid, main = "Studentized Residual Distribution")
+# Exclude studentized residual > 3
+rmv_stud_resid <- stud_resid[abs(stud_resid) > 2]
+length(rmv_stud_resid)
 
-#It turns out that the absolute residuals are not predicted very well by Yˆi
-#Hence, we conclude that there does not seem to be a problem with the constant variance assumption.
 
-#Test for Normality
-hist(residuals(model))
-boxplot(residuals(model))
+# Exclude outliers
+df_exc_out <- df[!row.names(df) %in% names(rmv_stud_resid),]
+lm.2 <- lm(modelForm, data = df_exc_out)
+# Residual analysis:
+par(mfrow = c(2, 2))
+plot(lm.2)
 
-#From the plots it seems like the residuals are normally distributed with mean of 0
+# Reset rownames
+rownames(df) <- seq(length=nrow(df))
+rownames(df_exc_out) <- seq(length=nrow(df_exc_out))
 
-#Shapiro.Wilks Normality Test
-shapiro.test(residuals(model))
-#The null hypothesis is that the residuals have a normal distribution. The p-value of the test
-#statistic is large in this example. It thus follows that the null hypothesis is not rejected.
+# Remove influential observations using Cook's D
+cook_simple = cooks.distance(lm.1)
+cook_exc_outliers = cooks.distance(lm.2)
+# identify Cook's D values > 4/(n-k-1) 
+cutoff <- 4/((nrow(df_exc_out)-length(lm.2$coefficients)-2))
+par(mfrow = c(1, 2))
+plot(as.integer(rownames(df)), cook_simple, main = "Including Outliers", 
+     ylab = "Cook's Distance", ylim = c(0, max(cook_simple)))
+abline(h = 0.005)
+plot(as.integer(rownames(df_exc_out)), cook_exc_outliers, main = "Excluding Outliers", 
+     ylab = "", ylim = c(0, max(cook_simple)))
+abline(h = 0.005)
 
-# to check for serial correlation
-#Durbin-Watson Test
-dwtest(model,data=df)
-# Th null hypothesis is there is no correlation among errors. The p value indicates that there is no evidence of correlated errors, 
+# Find Cook on top of data without outliers
+rmv_cook_inf <- cook_exc_outliers[cook_exc_outliers > cutoff]
+length(rmv_cook_inf)
+
+# Exclude Influence
+df_exc_inf <- df_exc_out[!row.names(df_exc_out) %in% names(rmv_cook_inf),]
+lm.3 <- lm(modelForm, data = df_exc_inf)
+# Residual analysis:
+par(mfrow = c(2, 2))
+plot(lm.3)
+
+
+# Scale values to reduce Heteroskedasticity
+str(df_exc_inf)
+df_exc_scalex <- df_exc_inf
+# We can scale only x variables to ln (Population and median.household.income) and verify
+df_exc_scalex$Population <- log(df_exc_scalex$Population, exp(1))
+df_exc_scalex$median.household.income <- log(df_exc_scalex$median.household.income, exp(1))
+lm.4 <- lm(modelForm, data = df_exc_scalex)
+# Residual analysis:
+par(mfrow = c(2, 2))
+plot(lm.4)
+
+# We now can scale only y to ln (YPLL.Rate) and verify
+df_exc_scaley <- df_exc_inf
+df_exc_scaley$YPLL.Rate <- log(df_exc_scaley$YPLL.Rate, exp(1))
+lm.5 <- lm(modelForm, data = df_exc_scaley)
+# Residual analysis:
+par(mfrow = c(2, 2))
+plot(lm.5) 
+
+
+# We now can scale both x and y to ln and verify
+df_exc_scalexy <- df_exc_inf
+df_exc_scalexy$Population <- log(df_exc_scalexy$Population, exp(1))
+df_exc_scalexy$median.household.income <- log(df_exc_scalexy$median.household.income, exp(1))
+df_exc_scalexy$YPLL.Rate <- log(df_exc_scalexy$YPLL.Rate, exp(1))
+lm.6 <- lm(modelForm, data = df_exc_scalexy)
+# Residual analysis:
+par(mfrow = c(2, 2))
+plot(lm.6) 
 
 # Test stepwise
-model_step <- step(model, direction="both")
-summary(model_step)
+lm.7 <- step(lm.5, direction="both")
+# Residual analysis:
+par(mfrow = c(2, 2))
+plot(lm.7)
 
-plot(model_step)
+# We now know model 5 is the best. But does step wise improve model 5?
+# Compare lm 7 with lm 5 as it is built on top of it.
+# Reduction in the residual sum of squares are statistically significant or not)
+anova(lm.5,lm.7, test="Chisq")
 
-#Test for constant variance
-summary(lm(abs(residuals(model_step)) ~ fitted(model_step)))
+# We choose model 7 as the best model as it gives the same performance with less features
+summary(lm.5)
+summary(lm.7)
 
-#It turns out that the absolute residuals are not predicted very well by Yˆi
-#Hence, we conclude that there does not seem to be a problem with the constant variance assumption.
-
-#Test for Normality
-hist(residuals(model_step))
-boxplot(residuals(model_step))
-
-#From the plots it seems like the residuals are normally distributed with mean of 0
-
-#Shapiro.Wilks Normality Test
-shapiro.test(residuals(model_step))
-#The null hypothesis is that the residuals have a normal distribution. The p-value of the test
-#statistic is large in this example. It thus follows that the null hypothesis is not rejected.
-
-
-# Fit model with transformation
-logTransformModelForm <- createModelFormula(yVar = 'log(YPLL.Rate)', xVars = xVars, includeIntercept = TRUE)
-logTransformModelForm
-
-logTransform <- lm(logTransformModelForm, data = df)
-#summary(glm(modelForm,data=df))
-summary(logTransform)
-plot(logTransform)
-
-#Test for constant variance
-summary(lm(abs(residuals(logTransform)) ~ fitted(logTransform)))
-
-#It turns out that the absolute residuals are not predicted very well by Yˆi
-#Hence, we conclude that there does not seem to be a problem with the constant variance assumption.
-
-#Test for Normality
-hist(residuals(logTransform))
-boxplot(residuals(logTransform))
-
-#From the plots it seems like the residuals are normally distributed with mean of 0
-
-#Shapiro.Wilks Normality Test
-shapiro.test(residuals(logTransform))
-#The null hypothesis is that the residuals have a normal distribution. The p-value of the test
-#statistic is large in this example. It thus follows that the null hypothesis is not rejected.
-
-# to check for serial correlation 
-#Durbin-Watson Test
-#install.packages('lmtest')
-library(lmtest)
-dwtest(logTransform,data=df)
-# The null hypothesis is there is no correlation among errors. The p value indicates that there is no evidence of correlated errors, 
-
-
-#Leverage
-
-#hatvalues(model)
-
-hv <- as.data.frame(hatvalues(model))
-hvmean <-mean(hatvalues(model))
-hv$warn <- ifelse(hv[, 'hatvalues(model)']>3*hvmean, 'x3',
-                  ifelse(hv[, 'hatvalues(model)']>2*hvmean, 'x2', '-' ))
-
-plot(hatvalues(model), type = "h")
-
-x3<- which(hv$warn == "x3")
-x2 <- which(hv$warn %in%c("x2", "x3"))
-
-leveragex3subsetdf <- df[-x3,]
-hv3model <- lm(modelForm, data = leveragex3subsetdf)
-summary(hv3model)
-plot(hv3model)
-
-
-leveragex2subsetdf <- df[-x2,]
-hv2model <- lm(modelForm, data = leveragex2subsetdf)
-summary(hv2model)
-plot(hv2model)
-
-
-
-#Inluence TestCooks D
-#cooks.distance(model)
-
-cooksD <- as.data.frame(cooks.distance(model))
-cooksmean <-mean(cooks.distance(model))
-cooksD$warn <- ifelse(cooksD[, 'cooks.distance(model)']>4*cooksmean, 'x4','-' )
-
-plot(cooksD$`cooks.distance(model)`,pch="*", cex=2, main="Influential Obs by Cooks distance")
-abline(h = 4*cooksmean, col="red")
-x4<- which(cooksD$warn == "x4")
-
-inluencex4subsetdf <- df[-x4,]
-cooksmodel <- lm(modelForm, data = inluencex4subsetdf)
-summary(cooksmodel)
-plot(cooksmodel)
 
